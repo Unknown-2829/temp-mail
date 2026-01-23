a4c48ff819c09540c4f3bbac3979e4751d510a17
ひղкͥnͣoͫฬภ <160002355+Unknown-2829@users.noreply.github.com>
2026-01-23 13:28:20 +0530

    Revert loading animation to lighter style per user request

--- public/app.js
+++ public/app.js
@@ -115,15 +115,13 @@ function renderInbox() {
         <div class="loading-animation">
           <svg class="loading-svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <g class="arrows-ring">
-               <path d="M50 10 A 40 40 0 0 1 90 50 L 82 50 A 32 32 0 0 0 50 18 Z" /> 
-               <path d="M90 50 L 96 46 L 82 36 L 75 46 L 82 50 Z" />
-               <path d="M50 90 A 40 40 0 0 1 10 50 L 18 50 A 32 32 0 0 0 50 82 Z" />
-               <path d="M10 50 L 4 54 L 18 64 L 25 54 L 18 50 Z" />
+               <path d="M50 10 A 40 40 0 0 1 85 30 L 90 25 L 90 40 L 75 40 L 80 35 A 35 35 0 0 0 50 15 Z" />
+               <path d="M50 90 A 40 40 0 0 1 15 70 L 10 75 L 10 60 L 25 60 L 20 65 A 35 35 0 0 0 50 85 Z" />
             </g>
-            <g class="envelope-icon" transform="translate(22.5, 23.5) scale(0.55)">
-                <path d="M10,80 L90,80 L90,40 L50,65 L10,40 Z" fill="#7a7d8f"/>
-                <path d="M10,30 L50,55 L90,30 L50,5 Z" fill="#5c5f70"/>
-                <rect x="10" y="30" width="80" height="50" rx="4" fill-opacity="0.1"/> 
+            <g class="envelope-icon" transform="translate(28, 35) scale(0.45)">
+                <path d="M10,80 L90,80 L90,40 L50,65 L10,40 Z" fill="#D1D4DE"/>
+                <path d="M10,30 L50,55 L90,30 L50,5 Z" fill="#9FA3B5"/>
+                <rect x="10" y="30" width="80" height="50" rx="5" fill-opacity="0.2"/> 
             </g>
           </svg>
         </div>
